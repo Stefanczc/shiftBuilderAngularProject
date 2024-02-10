2329fd2f66ea74dc05202fe9b87a6de4471ccbfa
Stefan Alexandru <stefanalecsandru@yahoo.com>
2024-02-10 22:50:32 +0200

    other changes
    test

--- src/app/shared/guards/admin-only.guard.ts
+++ src/app/shared/guards/admin-only.guard.ts
@@ -3,14 +3,12 @@ import { inject } from '@angular/core';
 import { ShareDataService } from '../services/share-data.service';
 import { ROUTE_MYSHIFTS } from 'src/app/app-routing.module';
 import { AuthService } from '../services/authentication.service';
-import { ModalService } from '../services/modal.service';
 
 export const adminOnlyGuard: CanActivateFn = async (route, state) => {
 
   const shareDataService = inject(ShareDataService);
   const router = inject(Router);
   const authService = inject(AuthService);
-  const modalService = inject(ModalService)
 
   authService.uid$.subscribe(async userId => {
     if (userId !== null) {
